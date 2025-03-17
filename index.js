c5c7f716a918914ebcba3f867c6991bee596fc0a
Afzal14786 <mdafzal14777@gmail.com>
2025-03-18 01:07:57 +0530

    implementing routing and connecting real time data and update it accordingly

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const {faker} = require("@faker-js/faker");
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const { connect } = require("http2");
+
+app.use(express.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+
+
 
 // this is how to create a connection nodejs to sql
 const connection = mysql.createConnection({
@@ -9,107 +22,219 @@ const connection = mysql.createConnection({
     password : "root"
 });
 
-// simple query 
 
-// let insert something into customer table
-
-// let q = "insert into customer (id, username, email, password) values (?, ?, ?, ?)";
-
-// this is one way to insert data using faker package
-
-// let valu = [faker.string.uuid(), faker.internet.username(), faker.internet.email(), faker.internet.password()];
 
 /**
- * we can also insert data manually ..
+ * '/' means home route.
+ * implementing the home route such a way, it display the total number of users. . .
+ */
+let count;
+app.get("/", (req, res)=> {
+    // this is the sql command.
+    let que = `select count(*) from user2`;
+    try {
+        connection.query(que, (err, result)=> {
+            if (err)
+                throw err;
+
+            count = result[0]["count(*)"];
+            res.render("home.ejs", {count});
+        });
+    } catch (err) {
+        res.send(`Some error is DB : ${err}`);
+    }
+});
+
+/**
+ * implementing 
+ * "/users"     fetch and show userid username and email,
  */
 
-// let insetIntoUser = "insert into users (id, name, email, phoneNo, city, state) values ?";
 
-// let userData = [
-//     [101, "Md Afzal Ansari", "ansari.afzal@edupoint.edu", 9955118287, "Mumbai", "Maharashtra"],
-//     [102, "Md Firoz Alam", "alam.firoz@edupoint.edu", 8256987690 ,"Ranchi", "Jharkhand"],
-//     [103, "Gulam Rabbani", "glg.rabbani@edupoint.edu", 7689564976, "Chandigarh", "Punjab"],
-//     [104, "Haider Ali", "ali.haider@edupoint.edu", 7985785423, "Bhagalpur", "Bihar"],
-//     [105, "Md Faizal Ansari", "faizal@edupoint.edu", 9985784767, "Sabor", "Bihar"],
-//     [106, "Kajal Yadhuvashi", "yadhu.kajal@edupoint.edu", 8790577865, "Kanpur", "Uttar Pradesh"],
-//     [107, "Nisha Poddar", "poddar.nisha@edupoint.edu", 8976347865, "Kolkata", "West Bengal"],
-//     [108, "Shweta Mahto", "mahto.shweta@edupoint.edu", 9087998456, "New Delhi", "Delhi"],
-//     [109, "Rakhi Verma", "verma.rakhi@edupoint.edu", 8945753589, "Madhubani", "Bihar"],
-//     [110, "Abid Ansari", "ansari.abid@edupoint.edu", 7865908765, "Godda", "Jharkhand"],
-//     [111, "Sammi Ansari", "ansari.sammi@edupoint.edu", 9981325645, "Bokaro", "Jharkhand"],
-//     [112, "Saloni Bhagat", "bhagat.saloni@edupoint.edu", 8976356754, "Ranchi", "Jharkhand"]
-// ];
+app.get("/users", (req, res) => {
+    let que = `select * from user2`;
+    try {
+        connection.query(que, (err, users)=> {
+            if (err)
+                throw err;
+            res.render("user.ejs", {users, count});
 
-// try {
+        })
+    } catch (err) {
+        res.send(`Some error in the database. ${err}`);
+    }
+});
 
-//     connection.query(insetIntoUser, [userData], (err, res)=> {
-//         if(err)
-//             throw err;
+/**
+ * implementing 
+ * /student-data  => get all the students information.
+ */
 
-//         console.log(res);
-//     });
+app.get("/student", (req, res) => {
+    let que = `select * from users`;
+    try {
+        connection.query(que, (err, studentData)=> {
+            if (err)
+                throw err;
 
-// } catch (err) {
-//     console.log(err);
-// }
+            res.render("studentdata.ejs", {studentData});
+        });
+    } catch(err) {
+        console.log(`Some error in database : ${err}`);
+    }
+});
+
+/**
+ * let implement edit users data like username
+ */
+
+app.get("/user/:id/edit", (req, res)=> {
+    let {id} = req.params;
+    let q = `select * from user2 where id = '${id}'`;
+
+    try {
+        connection.query(q, (err, result)=> {
+            if (err)
+                throw err;
+
+            let user = result[0];
+            res.render("edit.ejs", {user});
+        })
+    } catch(err) {
+        console.log(`Some error in DB ${err}`);
+    }
+});
+
+/**
+ * UPDATE THE DATA INTO DB
+ */
+
+app.patch("/user/:id", (req, res)=> {
+    let {id} = req.params;
+    let {password : formPassword, username : newUsername} = req.body;
+
+    let q = `select * from user2 where id = '${id}'`;
+
+    try {
+        connection.query(q, (err, result)=> {
+            if (err)
+                throw err;
+
+            let user = result[0];
+
+            if (formPassword !== user.password) {
+                res.send("WRONG PASSWORD");
+            } else {
+                let updateQ = `update user2 set username ='${newUsername}' where id ='${id}'`;
+                connection.query(updateQ, (err, result2)=> {
+                    if (err)
+                        throw err;
+                    res.send(result2);
+                });
+            }
+        });
+    } catch(err) {
+        console.log(`Error in the database ${err}`);
+    }
+});
+
+
+/**
+ * let's implementing students data
+ * 
+ */
+
+app.get("/student/:id/edit", (req, res)=> {
+    let {id} = req.params;
+    let q = `select * from users where id = '${id}'`;
+
+    try {
+        connection.query(q, (err, result)=> {
+            if (err)
+                throw err;
+
+            let student = result[0];
+            res.render("updateStudent.ejs", {student});
+        });
+    } catch (err) {
+        console.err(`Something wrong with the DB : ${err}`);
+    }
+});
+
+/**
+ * do it by today : March 18, 2025
+ */
+
+app.patch("student/:id", (req, res)=> {
+    let id = req.params;
+    let {username : newUser, pass1 : password1, pass2 : password2} = req.body;
+    console.log(req.body);
+    let q = `select * from users where id = '${id}'`;
+
+    try {
+        connection.query(q, (err, result)=> {
+            if (err)
+                throw err;
+
+            let student = result[0];
+
+            if (pass1 != pass2) {
+                console.log(result);
+                res.send("password doesn't match, please enter again . . .");
+            } else {
+                let updateQuery = `update users set username = '${newUser}' where id = '${id}'`;
+                connection.query(updateQuery, (err, result)=> {
+                    if (err)
+                        throw err;
+
+                    res.send(result);
+                });
+            }
+
+        });
+    } catch(err) {
+        console.log(`Something wrong with database : ${err}`);
+    }
+});
+
+
 
 
 /**
  * Let insert data into bulk using "@faker-js/faker" package
  */
 
-let getRandomData = () => {
-    return [
-        faker.string.uuid(),
-        faker.internet.username(),
-        faker.internet.email(),
-        faker.internet.password()
-    ]
-};
-
-let query_bulk = "insert into user2 (id, username, email, password) values ?";
-
-let data = [];
-
-for (let i = 0; i < 100; ++i) {
-    data.push(getRandomData());
-}
-
-try {
-    connection.query(query_bulk, [data], (err, res) => {
-        if (err)
-            throw err;
-
-        console.log(res);
-    });
-} catch(err) {
-    console.log(err);
-}
-
-connection.end();
-
-// let createRandomUser = () => {
-//     return {
-//       userId: faker.string.uuid(),
-//       username: faker.internet.username(), // before version 9.1.0, use userName()
-//       email: faker.internet.email(),
-//       avatar: faker.image.avatar(),
-//       password: faker.internet.password(),
-//       birthdate: faker.date.birthdate(),
-//       registeredAt: faker.date.past(),
-//     };
-// }
-
-// let getRandomUser = () => {
-//     return {
-//         id : faker.string.uuid(),
-//         username : faker.internet.username(),
-//         email : faker.internet.email(),
-//         password : faker.internet.password(),
-//     };
+// let getRandomData = () => {
+//     return [
+//         faker.string.uuid(),
+//         faker.internet.username(),
+//         faker.internet.email(),
+//         faker.internet.password()
+//     ]
 // };
 
-  
-// console.log(createRandomUser());
+// let query_bulk = "insert into user2 (id, username, email, password) values ?";
 
-// console.log(getRandomUser());
+// let data = [];
+
+// for (let i = 0; i < 100; ++i) {
+//     data.push(getRandomData());
+// }
+
+// try {
+//     connection.query(query_bulk, [data], (err, res) => {
+//         if (err)
+//             throw err;
+
+//         console.log(res);
+//     });
+// } catch(err) {
+//     console.log(err);
+// }
+
+// connection.end();
+
+
+app.listen("8080", ()=> {
+    console.log("Server is listening to port 8080.");
+});
